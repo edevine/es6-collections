45b4229053e497a4d1f77687dbb9cdca1e0adc35
Eric Devine <devineej@gmail.com>
2015-03-26 22:17:06 -0400

    Implemented Array.prototype.copyWithin

--- es6-collections.js
+++ es6-collections.js
@@ -194,45 +194,20 @@
 
     def(Array.prototype, {
         //Array#copyWithin
-        //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/copyWithin#Polyfill
         copyWithin: function copyWithin(target, start/*, end*/) {
-            // Steps 1-2.
-            if (this == null) {
-                throw new TypeError('this is null or not defined');
-            }
+            var end = arguments[2],
+                self = toObject(this),
+                len = toLen(self.length);
 
-            var O = Object(this);
+            target = target >> 0;
+            start = start >> 0;
+            end = isDef(end) ? end >> 0 : len;
 
-            // Steps 3-5.
-            var len = O.length >>> 0;
-
-            // Steps 6-8.
-            var relativeTarget = target >> 0;
-
-            var to = relativeTarget < 0 ?
-              Math.max(len + relativeTarget, 0) :
-              Math.min(relativeTarget, len);
-
-            // Steps 9-11.
-            var relativeStart = start >> 0;
-
-            var from = relativeStart < 0 ?
-              Math.max(len + relativeStart, 0) :
-              Math.min(relativeStart, len);
-
-            // Steps 12-14.
-            var end = arguments[2];
-            var relativeEnd = end === undefined ? len : end >> 0;
-
-            var final = relativeEnd < 0 ?
-              Math.max(len + relativeEnd, 0) :
-              Math.min(relativeEnd, len);
-
-            // Step 15.
-            var count = Math.min(final - from, len - to);
-
-            // Steps 16-17.
-            var direction = 1;
+            var to = target < 0 ? max(len + target, 0) : min(target, len),
+                from = start < 0 ? max(len + start, 0) : min(start, len)
+                final = end < 0 ? max(len + end, 0) : min(end, len),
+                count = min(final - from, len - to),
+                direction = 1;
 
             if (from < to && to < (from + count)) {
                 direction = -1;
@@ -240,21 +215,18 @@
                 to += count - 1;
             }
 
-            // Step 18.
             while (count > 0) {
-                if (from in O) {
-                    O[to] = O[from];
-                } else {
-                    delete O[to];
-                }
+                if (from in self)
+                    self[to] = self[from];
+                else
+                    delete self[to];
 
                 from += direction;
                 to += direction;
                 count--;
             }
 
-            // Step 19.
-            return O;
+            return self;
         },
 
         //Array#fill
